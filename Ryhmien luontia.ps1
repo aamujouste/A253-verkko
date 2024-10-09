a1e1fa416e1c1eb5890df90c3fe0976193cdbafe
Aamu <142008834+aamujouste@users.noreply.github.com>
2024-10-09 14:29:37 +0300

    Add files via upload

---
+++ Ryhmien luontia.ps1
@@ -0,0 +1,8 @@
+﻿# Luetaan AD:n käyttäjätilit ja tallennetaan CSV-tiedostoksi
+# ===========================================================
+
+# Määritellään tiedoston nimi ja polku
+$Tiedosto = "C:\Users\Administrator\Documents\Kaikkikäyttäjät2.csv"
+
+# Luetaan AD:n tiedot ja tallennetaan ne tiedostoon, välimerkiksi ; ja koodaus Unicode Exceliä varten
+Get-ADUser -Filter * -Properties Description | Export-Csv -Path $Tiedosto -Delimiter ";" -Encoding Unicode
