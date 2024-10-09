24325bd81aa7685d2d8759ae2fd8194a98cb9665
Aamu <142008834+aamujouste@users.noreply.github.com>
2024-10-09 14:29:55 +0300

    Add files via upload

---
+++ Käyttäjien luonti.ps1
@@ -0,0 +1,13 @@
+﻿#LISÄTÄÄN UUSIA AD-KÄYTTÄJIÄ CSV-TIEDOSTOSTA
+
+# Tiedoston polku, josta käyttäjätiedot löytyvät
+$Tiedosto = "C:\Users\Administrator\Documents\nimiä.csv"
+
+# Muodostetaan lista lisättävistä käyttäjistä lukemalla tiedosto
+$Käyttäjälista = Import-Csv -Delimiter ";" -Path $Tiedosto -Encoding Default
+
+# Kryptataan ensimmäinen salasana
+$Salasana = ConvertTo-SecureString -String "Q2werty7" -AsPlainText -Force
+
+# Putkitetaan lista käyttäjienluontikomennolle
+$Käyttäjälista | New-ADUser -AccountPassword $Salasana -ChangePasswordAtLogon 1 -Enabled 1
---
+++ Käyttäjien ryhmitys.ps1
@@ -0,0 +1,23 @@
+﻿# LISÄÄ KÄYTTÄJÄT RYHMÄÄN CSV-TIEDOSTOON
+# ======================================
+$Tiedosto = "C:\Users\Administrator\Documents\Johtoryhmanjasenet.csv"
+
+#Ryhmän nimi
+$Ryhmä = "Johto"
+
+# Lista ryhmän jäsenistä
+$Lista = Import-Csv -Path $Tiedosto -Delimiter ";" -Encoding Default
+
+# Laskuri joka näyttää lisättyjen jäsenten määrän
+$Laskuri = 0
+
+# Putkitetaan lista ryhmän jäseneksi käyttämällä foreach-silmukkaa
+foreach($Rivi in $Lista)
+{
+    Add-ADGroupMember -Identity $Ryhmä -Members $Rivi.SamAccountName
+    $Laskuri = $Laskuri + 1
+}
+
+$Ilmoitus = "Ryhmään " + $Ryhmä + " lisättiin " + $Laskuri + " uutta jäsentä"
+
+Write-Warning $Ilmoitus
